d3765a13e8f62490d3fe1904dc70fe150690db49
Marek Charvát <marekgta233@gmail.com>
2021-12-23 22:56:18 +0100

    set up

--- live-zmena-css/js/script.js
+++ live-zmena-css/js/script.js
@@ -1,1 +1,15 @@
-s
+function swapStyleSheet(sheet) {
+    document.getElementById("pagestyle").setAttribute("href", sheet);  
+}
+
+function initate() {
+    var style1 = document.getElementById("stylesheet1");
+    var style2 = document.getElementById("stylesheet2");
+    var style3 = document.getElementById("stylesheet3");
+    var style4 = document.getElementById("stylesheet4");
+
+    style1.onclick = swapStyleSheet("styles/dark.css");
+    style2.onclick = swapStyleSheet("styles/light.css");
+    style3.onclick = swapStyleSheet("styles/teal.css");
+    style4.onclick = swapStyleSheet("styles/violet.css");
+}
